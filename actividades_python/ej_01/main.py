4141987316499df300c47f221ac055e559bedd5c
Marcos-Caleb-Martinez <marcoscalebmartinez@gmail.com>
2024-04-18 20:54:23 +0100

    feat: programa que prende un LED azul con boton

--- actividades_python/ej_01/main.py
+++ actividades_python/ej_01/main.py
@@ -2,11 +2,13 @@
 from gpiozero import LED, Button
 from signal import pause
 
-led = LED (26)
-button= Button (18)
+# Configuración del LED en el pin GPIO 26 y del botón en el pin GPIO 18
+led = LED(26)
+button = Button(18)
 
+# Asignación de las funciones de devolución de llamada a los eventos del botón
 button.when_pressed = led.on
-button.when_released= led.off
+button.when_released = led.off
 
-pause()
-
+# Mantener el programa en ejecución hasta recibir una señal 
+pause ()
